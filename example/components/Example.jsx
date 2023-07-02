c4632f362ec4536ad13cdfa059099886da66db1d
Ibuki-Narukawa <ibuki0212job@gmail.com>
2023-07-02 17:37:08 +0900

    npm run startで動作確認

--- example/components/Example.jsx
+++ example/components/Example.jsx
@@ -18,6 +18,16 @@ const steps = [
   {
     id: '1',
     message: 'Hello World',
+    trigger: '2'
+  },
+  {
+    id: '2',
+    user: true,
+    trigger: '3'
+  },
+  {
+    id: '3',
+    message: 'Hi {previousValue}, nice to meet you!',
     end: true
   }
 ];
